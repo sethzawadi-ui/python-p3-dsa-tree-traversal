7e95110ad79db0511161bc4bd8f132ff91b10506
sethzawadi-ui <seth.zawadi@student.moringaschool.com>
2025-12-06 00:05:09 +0300

    dsa tree traversal

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,17 @@
 class Tree:
-  def __init__(self, root = None):
-    self.root = root
+    def __init__(self, root=None):
+        self.root = root
 
-  def get_element_by_id(self, id):
-    pass
+    def get_element_by_id(self, target_id):
+        def dfs(node):
+            if node is None:
+                return None
+            if node.get('id') == target_id:
+                return node
+            for child in node.get('children', []):
+                result = dfs(child)
+                if result:
+                    return result
+            return None
+
+        return dfs(self.root)
